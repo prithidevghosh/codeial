88ccf71387fa583ce1c13b0770a198aa2eec9144
Prithidev <prithidevghosh@gmail.com>
2022-12-01 11:23:08 +0530

    setting up route & controller for users

--- route/index.js
+++ route/index.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const homeController=require('../controller/home_controller')
+const users=require('./users')
 const route=express.Router('');
 
 route.get('/',homeController.home);
-
+route.use('/users',users);
 module.exports=route;
